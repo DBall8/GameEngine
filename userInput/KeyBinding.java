2e52b4aede202432a454a909dd877b3df04a0265
Damon Ball <dcball@wpi.edu>
2019-04-07 16:22:49 -0400

    Adding press and release events

--- userInput/KeyBinding.java
+++ userInput/KeyBinding.java
@@ -1,17 +1,45 @@
 package gameEngine.userInput;
 
+import gameEngine.callback.Callback;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  * Class for tracking if a keyboard key is currently pressed or not
  */
 public class KeyBinding extends InputBinding {
     KeyCode key; // KeyCode being watched
+    private Callback<Void> onPressEvent = null;
+    private Callback<Void> onReleaseEvent = null;
 
     KeyBinding(KeyCode key)
     {
         this.key = key;
     }
 
+    @Override
+    void setPressed(boolean isPressed)
+    {
+        super.setPressed(isPressed);
+        if(isPressed && onPressEvent != null)
+        {
+            onPressEvent.run(null);
+        }
+        else if(!isPressed && onReleaseEvent != null)
+        {
+            onReleaseEvent.run(null);
+        }
+    }
+
     public KeyCode getKey(){ return key; }
+
+    public void setOnPressEvent(Callback<Void> onPressEvent)
+    {
+        this.onPressEvent = onPressEvent;
+    }
+
+    public void setOnReleaseEvent(Callback<Void> onReleaseEvent)
+    {
+        this.onReleaseEvent = onReleaseEvent;
+    }
 }
